77189971c9b69360dcf49400222f278e5d73818d
brGuirra <br.guirra@gmail.com>
2022-01-11 23:17:02 -0300

    project completion

--- project-1/DetailViewController.swift
+++ project-1/DetailViewController.swift
@@ -8,14 +8,53 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    var pictureIndex: Int?
+    var amountOfPictures: Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let pictureIndex = pictureIndex, let amountOfPictures = amountOfPictures {
+            title = "Picture \(pictureIndex) of \(amountOfPictures)"
+        }
+        navigationItem.largeTitleDisplayMode = .never
+        
+        // Add an action button the navigation bar,
+        // the type action is to indicate the appearance
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
 
-        // Do any additional setup after loading the view.
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("picture not found.")
+            return
+        }
+        
+        
+        let vc = UIActivityViewController(activityItems: [selectedImage ?? "", image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+    
     /*
     // MARK: - Navigation
 
